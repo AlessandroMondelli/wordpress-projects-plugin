46f59dd2a279043fdd109619cf47f26ab9cf21f3
Alessandro Mondelli <alexs98@hotmail.it>
2022-06-13 12:58:03 +0200

    Aggiunte taxonomies a Custom post type per filtro front-end (ad aggiungere a prossima push)

--- inc/class-project-functions.php
+++ inc/class-project-functions.php
@@ -9,7 +9,9 @@ class AmProjectFunctions {
     }
 
     protected function set_hooks() {
-        add_action( 'init', [$this, 'am_register_projects' ] );
+        add_action( 'init', [ $this, 'am_register_projects' ] );
+        add_action( 'init', [ $this, 'am_register_taxonomies' ] );
+
         add_shortcode( 'am_projects_shortcode', [ $this, 'am_projects_html' ] );
     }
 
@@ -30,6 +32,58 @@ class AmProjectFunctions {
         );
 
         flush_rewrite_rules();
+    }
+
+    //Nuove taxonomies
+    function am_register_taxonomies() {
+        $labels_discipl = array(
+            'name'              => _x( 'Discipline', 'Discipline progetto' ),
+            'singular_name'     => _x( 'Disciplina', 'Disciplina progetto' ),
+            'search_items'      => __( 'Cerca disciplina' ),
+            'all_items'         => __( 'Tutte le discipline' ),
+            'parent_item'       => __( 'Disciplina padre' ),
+            'parent_item_colon' => __( 'Disciplina padre:' ),
+            'edit_item'         => __( 'Modifica disciplina' ),
+            'update_item'       => __( 'Aggiorna disciplina' ),
+            'add_new_item'      => __( 'Aggiungi nuova disciplina' ),
+            'new_item_name'     => __( 'Nome nuova disciplina' ),
+            'menu_name'         => __( 'Discipline' ),
+        );
+
+        $args_discipl   = array(
+            'hierarchical'      => true,
+            'labels'            => $labels_discipl,
+            'show_ui'           => true,
+            'show_admin_column' => true,
+            'show_in_rest'      => true,
+            'query_var'         => true,
+            'rewrite'           => [ 'slug' => 'disciplina' ],
+        );
+
+        $labels_years = array(
+            'name'              => _x( 'Anno', 'Anno progetto' ),
+            'singular_name'     => _x( 'Anno', 'Anno progetto' ),
+            'search_items'      => __( 'Cerca anno' ),
+            'all_items'         => __( 'Tutti gli anni' ),
+            'edit_item'         => __( 'Modifica anno' ),
+            'update_item'       => __( 'Aggiorna anno' ),
+            'add_new_item'      => __( 'Aggiungi nuovo anno' ),
+            'new_item_name'     => __( 'Nuovo anno' ),
+            'menu_name'         => __( 'Anni' ),
+        );
+
+        $args_years   = array(
+            'hierarchical'      => false,
+            'labels'            => $labels_years,
+            'show_ui'           => true,
+            'show_admin_column' => true,
+            'show_in_rest'      => true,
+            'query_var'         => true,
+            'rewrite'           => [ 'slug' => 'anno' ],
+        );
+
+        register_taxonomy( 'discipline', [ 'am_projects' ], $args_discipl );
+        register_taxonomy( 'anni', [ 'am_projects' ], $args_years );
     }
 
     public function am_projects_html() {
@@ -57,8 +111,8 @@ class AmProjectFunctions {
                 $pageImg = '';
             }
     
-            //$discipl = get_the_terms($project->ID, 'discipline'); //Prendo tag pagine progetti
-            //$anni = get_the_terms($project->ID, 'anni'); //Prendo tag pagine progetti
+            $discipl = get_the_terms($project->ID, 'discipline'); //Prendo tag pagine progetti
+            $anni = get_the_terms($project->ID, 'anni'); //Prendo tag pagine progetti
             
             if($j == 0 || $j % $n_elements_column == 0) {
                 $htmlStr .= '<div class="projects-col n-col-' . $n_col . ' amproj-col-sm-12 amproj-col-md-6 amproj-col-lg-4 amproj-col-xl-4 clearfix">'; 
@@ -68,20 +122,20 @@ class AmProjectFunctions {
             
             $htmlStr .= '<div class="amproj-inner';
             
-            //@TODO Aggiungere taxonomies per creare filtri front-end
-            // if( (isset($discipl) && (!empty($discipl))) ) {
-            //     for($i = 0; $i < count($discipl); $i++) { //Scorro array discipline
-            //         if($discipl[$i] != null && $discipl[$i] != "")
-            //             $htmlStr .= ' ' . $discipl[$i]->name; //Inserisco come classe
-            //     }
-            // }
+            //Stampo taxonomies come classi per filtro front-end
+            if( (isset($discipl) && (!empty($discipl))) ) {
+                for($i = 0; $i < count($discipl); $i++) { //Scorro array discipline
+                    if($discipl[$i] != null && $discipl[$i] != "")
+                        $htmlStr .= ' ' . $discipl[$i]->name; //Inserisco come classe
+                }
+            }
     
-            // if( (isset($anni) && (!empty($anni))) ) {
-            //     for($i = 0; $i < count($anni); $i++) { //Scorro array anni
-            //         if($anni[$i] != null && $anni[$i] != "")
-            //             $htmlStr .= ' ' . $anni[$i]->name; //Inserisco come classe
-            //     }
-            // }
+            if( (isset($anni) && (!empty($anni))) ) {
+                for($i = 0; $i < count($anni); $i++) { //Scorro array anni
+                    if($anni[$i] != null && $anni[$i] != "")
+                        $htmlStr .= ' ' . $anni[$i]->name; //Inserisco come classe
+                }
+            }
             
             if($pageImg != '') {
                 $htmlStr .= '"><div class="amproj-thumbnail"> <a href="' . $pageLink . '" class="amproj-thumbnail-link"><img class="amproj-thumbnail-img" src="' . $pageImg . '" data-src="' . $pageImg . '" alt="' . $pageTitle . '"></a></div>'; 
